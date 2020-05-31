5f86a0904d965a56a275673d0f19b849b76c1420
Mark Cox <mark.cox@confluent.io>
2020-05-31 14:04:01 -0700

    Create get_west.sh

---
+++ albertsons/get_west.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl -X POST -H "Content-Type: application/vnd.kafka.v2+json" --data '{"name": "albertsons_instance", "format": "binary", "auto.commit.enable": "true"}' http://ec2-54-193-82-232.us-west-1.compute.amazonaws.com:8082/consumers/java_consumer
+
+sleep 1
+
+curl -X POST -H "Content-Type: application/vnd.kafka.v2+json" --data '{"topics":["albertsons"]}' http://ec2-54-193-82-232.us-west-1.compute.amazonaws.com:8082/consumers/java_consumer/instances/albertsons_instance/subscription
+
+sleep 1
+
+echo
+echo
+
+curl -X GET -H "Accept: application/vnd.kafka.binary.v2+json" http://ec2-54-193-82-232.us-west-1.compute.amazonaws.com:8082/consumers/java_consumer/instances/albertsons_instance/records | jq
+
+echo
+echo
+
+sleep 1
+
+curl -X DELETE -H "Content-Type: application/vnd.kafka.v2+json" http://ec2-54-193-82-232.us-west-1.compute.amazonaws.com:8082/consumers/java_consumer/instances/albertsons_instance
